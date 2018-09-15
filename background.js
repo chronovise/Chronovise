3562b12d2137b68a0e18de232407ea47e02f6079
chronovise <chronovise@gmail.com>
2018-09-15 11:05:07 -0400

    First commit

---
+++ background.js
@@ -0,0 +1,8 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log("The color is green.");
+  });
+});
+
+chrome.browserAction.setBadgeText({text: 'ON'});
+chrome.browserAction.setBadgeBackgroundColor({color: '#4688F1'});
