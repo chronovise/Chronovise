706cebb3fd974d8af1dfaa05f2b59e3c26e2309f
chronovise <chronovise@gmail.com>
2018-09-15 16:19:10 -0400

    xd

--- popup.js
+++ popup.js
@@ -1,16 +1,36 @@
-sites = ["https://www.google.com","https://www.facebook.com"];
+work = ["https://www.google.com","https://www.facebook.com"];
+school = ["https://www.youtube.com"];
+home = [];
+travel = [];
+
 
 document.addEventListener('DOMContentLoaded', function(){
-  document.getElementById('button1').addEventListener('click',loadSites);
-  document.getElementById('button2').addEventListener('click',loadSites);
-  document.getElementById('button3').addEventListener('click',loadSites);
-  document.getElementById('button4').addEventListener('click',loadSites);
+  document.getElementById('button1').addEventListener('click',loadWork);
+  document.getElementById('button2').addEventListener('click',loadSchool);
+  document.getElementById('button3').addEventListener('click',loadHome);
+  document.getElementById('button4').addEventListener('click',loadTravel);
 });
 
-function loadSites(e){
-
-  for(i = 0;i<sites.length;i++){
-    chrome.tabs.create({url:sites[i], selected: false});
+function loadWork(e){
+  for(i = 0;i<work.length;i++){
+    chrome.tabs.create({url:work[i], selected: false});
   }
+}
 
+function loadSchool(e){
+  for(i = 0;i<work.length;i++){
+    chrome.tabs.create({url:school[i], selected: false});
+  }
+}
+
+function loadHome(e){
+  for(i = 0;i<home.length;i++){
+    chrome.tabs.create({url:home[i], selected: false});
+  }
+}
+
+function loadTravel(e){
+  for(i = 0;i<work.length;i++){
+    chrome.tabs.create({url:travel[i], selected: false});
+  }
 }
