08fd7e509cd6d0ceee5c439196891a3aa6d3f67a
chronovise <chronovise@gmail.com>
2018-09-15 21:48:48 -0400

    saving

--- background.js
+++ background.js
@@ -3,12 +3,3 @@ chrome.runtime.onInstalled.addListener(function() {
     console.log("The color is green.");
   });
 });
-
-document.getElementById('saveWork').onclick = function(){
-  var value = document.getElementById('content').firstChild.value.split("\n");
-  chrome.storage.sync.set({'work': value},function(){
-    console.log("value saved "+value);
-  });
-}
-
-Website/chronoviseHome.html
